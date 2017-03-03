901597a0d17c3d365c1d731593bbfd17cc395f2c
Wojciech Panek <wpanek@apptension.com>
2017-03-03 11:01:19 +0100

    Add AppVersion plugin

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -5,6 +5,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const WebpackAppversionPlugin = require('webpack-appversion-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
 
 const buildSpritePlugin = (name) => new SpritesmithPlugin({
@@ -25,106 +26,121 @@ const buildSpritePlugin = (name) => new SpritesmithPlugin({
   },
 });
 
-module.exports = (options) => ({
-  entry: options.entry,
-  output: Object.assign({ // Compile into js/build.js
-    path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/',
-  }, options.output), // Merge with env dependent settings
-  module: {
-    loaders: [{
-      test: /\.js$/, // Transform all .js files required somewhere with Babel
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: options.babelQuery,
-    }, {
-      // Do not transform vendor's CSS with CSS-modules
-      // The point is that they remain in global scope.
-      // Since we require these CSS files in our JS or CSS files,
-      // they will be a part of our compilation either way.
-      // So, no need for ExtractTextPlugin here.
-      test: /\.css$/,
-      include: /node_modules/,
-      loaders: ['style-loader', 'css-loader'],
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader',
+module.exports = (options) => {
+  const webpackConfig = {
+    entry: options.entry,
+    output: Object.assign({ // Compile into js/build.js
+      path: path.resolve(process.cwd(), 'build'),
+      publicPath: '/',
+    }, options.output), // Merge with env dependent settings
+    module: {
+      loaders: [{
+        test: /\.js$/, // Transform all .js files required somewhere with Babel
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: options.babelQuery,
       }, {
-        loader: 'css-loader',
+        // Do not transform vendor's CSS with CSS-modules
+        // The point is that they remain in global scope.
+        // Since we require these CSS files in our JS or CSS files,
+        // they will be a part of our compilation either way.
+        // So, no need for ExtractTextPlugin here.
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
       }, {
-        loader: 'sass-loader',
-      }],
-    }, {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'file-loader',
-    }, {
-      test: /\.(jpg|png|gif)$/,
-      loaders: [
-        'file-loader',
-        {
-          loader: 'image-webpack-loader',
-          query: {
-            progressive: true,
-            optimizationLevel: 7,
-            interlaced: false,
-            pngquant: {
-              quality: '65-90',
-              speed: 4,
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loaders: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
             },
           },
+        ],
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.(mp4|webm)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
         },
-      ],
-    }, {
-      test: /\.html$/,
-      loader: 'html-loader',
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader',
-    }, {
-      test: /\.(mp4|webm)$/,
-      loader: 'url-loader',
-      query: {
-        limit: 10000,
-      },
-    }],
-  },
-  plugins: options.plugins.concat([
-    new FaviconsWebpackPlugin(path.join(process.cwd(), 'app', 'images', 'favicon.png')),
-    buildSpritePlugin('mobile'),
-    buildSpritePlugin('desktop'),
-    new webpack.ProvidePlugin({
-      // make fetch available
-      fetch: 'exports-loader?self.fetch!whatwg-fetch',
-    }),
-
-    // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
-    // inside your code for any environment checks; UglifyJS will automatically
-    // drop any unreachable code.
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-    new webpack.NamedModulesPlugin(),
-  ]),
-  resolve: {
-    alias: {
-      'env-config': path.join(process.cwd(), 'app', 'environment', `${process.env.NODE_ENV}.js`),
+      }],
     },
-    modules: ['app', 'node_modules'],
-    extensions: [
-      '.js',
-      '.jsx',
-      '.react.js',
-    ],
-    mainFields: [
-      'browser',
-      'jsnext:main',
-      'main',
-    ],
-  },
-  devtool: options.devtool,
-  target: 'web', // Make web variables accessible to webpack, e.g. window
-  performance: options.performance || {},
-});
+    plugins: options.plugins.concat([
+      new FaviconsWebpackPlugin(path.join(process.cwd(), 'app', 'images', 'favicon.png')),
+      buildSpritePlugin('mobile'),
+      buildSpritePlugin('desktop'),
+      new webpack.ProvidePlugin({
+        // make fetch available
+        fetch: 'exports-loader?self.fetch!whatwg-fetch',
+      }),
+
+      // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
+      // inside your code for any environment checks; UglifyJS will automatically
+      // drop any unreachable code.
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        },
+      }),
+      new webpack.NamedModulesPlugin(),
+    ]),
+    resolve: {
+      alias: {
+        'env-config': path.join(process.cwd(), 'app', 'environment', `${process.env.NODE_ENV}.js`),
+      },
+      modules: ['app', 'node_modules'],
+      extensions: [
+        '.js',
+        '.jsx',
+        '.react.js',
+      ],
+      mainFields: [
+        'browser',
+        'jsnext:main',
+        'main',
+      ],
+    },
+    devtool: options.devtool,
+    target: 'web', // Make web variables accessible to webpack, e.g. window
+    performance: options.performance || {},
+  };
+
+  if (process.env.SHOW_VERSION) {
+    webpackConfig.plugins.push(
+      new WebpackAppversionPlugin({
+        entries: ['main'],
+        version: '1.0.0',
+        isOpen: true,
+      })
+    );
+  }
+
+  return webpackConfig;
+};
+
