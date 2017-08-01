eb3cef7059591a0c3c980be30161f6e7f71f2cec
Sergey Bolshov <bolszow@gmail.com>
2017-08-01 09:06:30 +0200

    Handle /404 route

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,9 +11,12 @@ export class RootContainer extends PureComponent {
     return (
       <Switch>
         <Route exact path="/" render={() => <Redirect to={DEFAULT_LOCALE} />} />
+
         <Route path="/:lang">
           <App>
             <Switch>
+              <Route exact path="/404" component={NotFound} />
+
               <Route exact path="/:lang" component={Home} />
 
               <Route component={NotFound} />
