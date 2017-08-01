93046a74e810e9f4fc86e6da57a116a7ff680d9e
Sergey Bolshov <bolszow@gmail.com>
2017-08-01 10:10:33 +0200

    Use prop-types package

--- app/routes/app.component.js
+++ app/routes/app.component.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { IntlProvider } from 'react-intl';
 import { get } from 'lodash';
--- app/routes/home/home.component.js
+++ app/routes/home/home.component.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import envConfig from 'env-config';
