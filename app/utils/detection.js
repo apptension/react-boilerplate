5a85584785e2da2c86d632e3509b018cfb026765
Martin Kmieciak <mkmieciak@apptension.com>
2017-11-03 11:18:55 +0100

    Show unsupported text

--- app/utils/detection.js
+++ app/utils/detection.js
@@ -64,7 +64,7 @@ export default class Detection {
     return type;
   }
 
-  static compareVersions(a, b) {
+  compareVersions(a, b) {
     if (typeof a === 'string' || a instanceof String) {
       return semverCompare(a, b) <= 0;
     }
