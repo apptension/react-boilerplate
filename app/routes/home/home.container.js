8f71d8c97bb59a952a41570ff3d4aeee02a84652
Patryk Ziemkowski <pziemkowski@apptension.com>
2018-03-02 17:04:25 +0100

    Add ramda to repository and use compose in containers

--- app/routes/home/home.container.js
+++ app/routes/home/home.container.js
@@ -2,12 +2,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import { hot } from 'react-hot-loader';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'ramda';
 
 import { Home } from './home.component';
 import { MaintainersActions } from '../../modules/maintainers/maintainers.redux';
 import { selectMaintainersItems } from '../../modules/maintainers/maintainers.selectors';
 import { LocalesActions } from '../../modules/locales/locales.redux';
 import { selectLocalesLanguage } from '../../modules/locales/locales.selectors';
+
 
 const mapStateToProps = createStructuredSelector({
   items: selectMaintainersItems,
@@ -19,4 +22,8 @@ export const mapDispatchToProps = (dispatch) => bindActionCreators({
   setLanguage: LocalesActions.setLanguage,
 }, dispatch);
 
-export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(Home));
+export default compose(
+  hot(module),
+  connect(mapStateToProps, mapDispatchToProps),
+  withRouter
+)(Home);
